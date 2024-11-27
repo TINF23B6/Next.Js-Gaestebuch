b2e110b0767edfbf565e3f474704d3160a16df0d
EinsEli <57264430+EinsEli@users.noreply.github.com>
2024-11-27 14:55:30 +0100

    Update theme provider

--- src/components/guestbook-card.tsx
+++ src/components/guestbook-card.tsx
@@ -22,43 +22,44 @@ export default function GuestbookCard() {
 		});
 	}, []);
 
-
 	return (
 		<Card>
-			<CardHeader
-				className="flex-row gap-4"
-			>
+			<CardHeader className="flex-row gap-4">
 				<Logo />
 				<CardTitle>TINF23B6 Gästebuch</CardTitle>
 			</CardHeader>
 			<CardContent className="flex flex-row gap-4 h-96">
-				<div
-					className="flex flex-col gap-4 w-72"
-				>
+				<div className="flex flex-col gap-4 w-72">
 					<Input
 						placeholder="Vorname / Nachname"
+						value={name}
 						onChange={(e) => setName(e.currentTarget.value)}
 					/>
 					<Input
 						placeholder="Nachricht"
+						value={message}
 						onChange={(e) => setMessage(e.currentTarget.value)}
 					/>
 					<Button
-						onClick={() => 
-							addEntry(name, message).then(() => {
-								getEntries().then((entries) => {
-									setEntries(entries);
+						onClick={() => {
+							if (name.length > 0 && message.length > 0) {
+								addEntry(name, message).then(() => {
+									getEntries().then((entries) => {
+										setEntries(entries);
+										setName("");
+										setMessage("");
+									});
 								});
-							})
-						}
-					>Absenden</Button>
+							}
+						}}
+					>
+						Absenden
+					</Button>
 				</div>
 				<Separator orientation="vertical" className="h-full" />
 				<div className="flex flex-col gap-4 overflow-y-auto w-96 items-center px-4">
 					{entries.length < 1 && (
-						<pre
-							className="text-muted-foreground text-sm"
-						>
+						<pre className="text-muted-foreground text-sm">
 							Hier ist es noch ziemlich leer...
 						</pre>
 					)}
@@ -68,7 +69,7 @@ export default function GuestbookCard() {
 							name={entry.name}
 							message={entry.message}
 						/>
-					))}					
+					))}
 				</div>
 			</CardContent>
 		</Card>
